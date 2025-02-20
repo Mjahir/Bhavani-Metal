bf2b6e8518c314bd654c2e334ad8c3444926bc2e
Mjahir <hathaliyamahesh2004@gmail.com>
2025-02-20 17:57:54 +0530

    Added GST number to website

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,8 @@ const Footer = () => {
       <footer className="bg-gray-800 text-white">
         <div className="container mx-auto px-4 py-6 text-center">
           <p>&copy; {new Date().getFullYear()} Jay Bhavani Metal Corporation. All rights reserved.</p>
+
+      <p>🧾 GST No: 24ACHPH3322D1ZA</p>
         </div>
       </footer>
     )
